8c62e6c6600878096ac805c41e75f74f9ace7c0e
Yashpal <yashpal.webinfinitesolutions@gmail.com>
2024-09-04 16:26:24 +0530

    changes in e2e file

--- e2e/example.spec.js
+++ e2e/example.spec.js
@@ -1,19 +1,12 @@
 // @ts-check
-const { test, expect } = require('@playwright/test');
+import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test('test', async ({ page }) => {
+  await page.goto('https://demo.playwright.dev/todomvc/');
+  await page.goto('https://demo.playwright.dev/todomvc/#/');
+  await page.getByPlaceholder('What needs to be done?').click();
+  await page.getByPlaceholder('What needs to be done?').fill('feed dog');
+  await page.getByPlaceholder('What needs to be done?').press('Enter');
+  await page.getByLabel('Toggle Todo').check();
+  await expect(page.getByText('1 items leftAll Active')).toBeVisible();
 });
