2742fb6ff45d07f83b9149e06db5bc6ebf06b61a
Yashpal <yashpal.webinfinitesolutions@gmail.com>
2024-09-05 09:58:28 +0530

    done

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,12 +1,19 @@
 // @ts-check
-import { test, expect } from '@playwright/test';
+const { test, expect } = require('@playwright/test');
 
-test('test', async ({ page }) => {
-  await page.goto('https://demo.playwright.dev/todomvc/');
-  await page.goto('https://demo.playwright.dev/todomvc/#/');
-  await page.getByPlaceholder('What needs to be done?').click();
-  await page.getByPlaceholder('What needs to be done?').fill('feed dog');
-  await page.getByPlaceholder('What needs to be done?').press('Enter');
-  await page.getByLabel('Toggle Todo').check();
-  await expect(page.getByText('1 items leftAll Active')).toBeVisible();
+test('has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
